4bfead0de4d937c6c6bfc72b325d074f93394d63
Pradneya <pradneya.prog@gmail.com>
2023-08-28 13:26:42 +0530

    Completed Stacks

--- Lecture_15_Stack/src/Solution.java
+++ Lecture_15_Stack/src/Solution.java
@@ -1,18 +1,17 @@
 import java.util.Stack;
 
 public class Solution {
-    public static int[] calculateSpan(int[] price, int n)
-    {
+    public static int[] calculateSpan(int[] price, int n) {
         // Your code here
         Stack<Integer> st = new Stack<>();
         int[] ans = new int[n];
-        for(int i = 0 ; i < n ; i++){
-            while(st.size() != 0 && price[i] >= price[st.peek()])
+        for (int i = 0; i < n; i++) {
+            while (st.size() != 0 && price[i] >= price[st.peek()])
                 st.pop();
-            if(st.size() == 0)
-                ans[i] = i+1;
-            else{
-                ans[i] = i-st.peek();
+            if (st.size() == 0)
+                ans[i] = i + 1;
+            else {
+                ans[i] = i - st.peek();
             }
             st.push(i);
         }
@@ -92,17 +91,17 @@ public class Solution {
 
         public void push(int val) {
             st.push(val);
-            if(min.size() == 0)
+            if (min.size() == 0)
                 min.push(val);
             else {
-                if(min.peek() > val )
+                if (min.peek() > val)
                     min.push(val);
             }
         }
 
         public void pop() {
             int x = st.pop();
-            if(x == min.peek())
+            if (x == min.peek())
                 min.pop();
         }
 
@@ -113,6 +112,36 @@ public class Solution {
         public int getMin() {
             return min.peek();
         }
+    }
+
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        Stack<Integer> st = new Stack<>();
+        int n = nums.length;
+        int[] ans = new int[n - k + 1];
+        int[] nge = new int[n];
+
+        for (int i = n - 1; i >= 0; i--) {
+            while (st.size() != 0 && nums[i] > nums[st.peek()])
+                st.pop();
+            if (st.size() == 0)
+                nge[i] = n;
+            else
+                nge[i] = st.peek();
+
+            st.push(i);
+        }
+
+        int j = 0;
+        for (int i = 0; i < n - k + 1; i++) {
+            if (j >= i + k) j = i;
+            int max = nums[j];
+            while (j < i + k) {
+                max = nums[j];
+                j = nge[j];
+            }
+            ans[i] = max;
+        }
+        return ans;
     }
 
     public static void main(String[] args) {
